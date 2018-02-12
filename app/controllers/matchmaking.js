5431367b73a21587b56dfa67760cdfd58743e8b1
Jacob Duyon <duyon.jacob@gmail.com>
2018-02-12 10:47:30 -0500

    added classes for managing queue data, and other client data the server needs to know. Also added an api endpoint which can be used with a post request (id and mmr data is needed).

---
+++ app/controllers/matchmaking.js
@@ -0,0 +1,182 @@
+const util = require('util') // remove this
+var queue = require('../structs/queue');
+var BINNED_QUEUES = queue.binRanges();
+var ENQUEUED_PLAYERS = new Set(); 
+var DEQUEUED_PLAYERS = {}
+var MMR_LOOKUP = queue.genKeyLookup();
+var PROPERTIES = new Object();
+PROPERTIES.MMR_LOOKUP = MMR_LOOKUP;
+PROPERTIES.BINNED_QUEUES = BINNED_QUEUES;
+PROPERTIES.ENQUEUED_PLAYERS = ENQUEUED_PLAYERS;
+PROPERTIES.DEQUEUED_PLAYERS = DEQUEUED_PLAYERS;
+
+class SearchingClientsManager{
+  constructor(properties){
+    this.properties = properties;
+  }
+  setEnqueued(id){
+    // Store people who are already on the queue.
+    this.properties.ENQUEUED_PLAYERS.add(id);
+  }
+  getEnqueued(id){
+    return this.properties.ENQUEUED_PLAYERS.has(id)
+  }
+  removeFromEnqueued(id){
+    delete this.properties.ENQUEUED_PLAYERS.delete(id);
+  }
+}
+
+class MatchPairingManager{
+  constructor(properties){
+    this.properties = properties;
+  }
+  setDequeued(id, opponent_id){
+    // Store dequeued player & requestor's id
+    this.properties.DEQUEUED_PLAYERS[opponent_id] = id
+  }
+  hasBeenDequeued(id){
+    return this.properties.DEQUEUED_PLAYERS[id];
+  }
+  removeFromDequeued(id){
+    delete this.properties.DEQUEUED_PLAYERS[id];
+  }
+
+}
+
+class DataStoreManager{
+  constructor(properties, searcher_manager, pairing_manager){
+    this.properties = properties;
+    this.searcher_manager = searcher_manager;
+    this.pairing_manager = pairing_manager;
+  }
+
+  createPlayerNode(id, mmr){
+    return new queue.Node({'id': id, 'mmr': mmr});
+  }
+  
+  getBinKey(mmr){
+    return this.properties.MMR_LOOKUP[mmr];
+  }
+  
+  getQueueHead(bin_key){
+    return this.properties.BINNED_QUEUES[bin_key].head
+  }
+  
+  dequeueFromBin(bin_key){
+    return this.properties.BINNED_QUEUES[bin_key].dequeue();
+  }
+
+  enqueueToBin(bin_key, player_node){
+    this.properties.BINNED_QUEUES[bin_key].enqueue(player_node);
+  }
+
+  isQueueHead(bin_key, id){
+    var top_of_queue = this.getQueueHead(bin_key);
+    if (top_of_queue && top_of_queue.data.id == id){
+      return true;
+    }
+  }
+
+  removeQueuedInfo(id){
+    /*
+      Remove players from matching/searching player data store.
+      Note: This doesn't remove players from the binned queue data structure.
+    */
+    this.pairing_manager.removeFromDequeued(id);
+    this.searcher_manager.removeFromEnqueued(id);
+  }
+  
+  getBinnedCompetitor(bin_key, id){
+    // If you are at the top of queue, don't dequeue yourself
+    // Otherwise you can be matched with yourself. This happens
+    // if you are the only one in the queue.
+    if (this.isQueueHead(bin_key, id)){
+      return;
+    }
+  
+    var dequeued = this.dequeueFromBin(bin_key);
+    if (dequeued){
+      console.log('You dequeued id: ', dequeued.data['id'])
+      // Add dequeud client to dequeued lookup as a key
+      this.pairing_manager.setDequeued(id, dequeued.data['id']);
+      // Add yourself to dequeud lookup as a key
+      // Note: The client never calls this when they are already queued.
+      this.pairing_manager.setDequeued(dequeued.data['id'], id);
+  
+      console.log('I found a matching player for you');
+      console.log('Im adding you to dequeued so that you cant be added to queue')
+    }
+    return dequeued
+  }
+
+  enqueueId(bin_key, id, mmr){
+    // Don't re-enqueue yourself
+    if (!this.searcher_manager.getEnqueued(id)){
+      this.searcher_manager.setEnqueued(id);
+      var player = this.createPlayerNode(id,mmr);
+      this.enqueueToBin(bin_key, player);
+      console.log('You have been enqueued');
+      return true;
+    }
+    else{
+      console.log('You are already queued!');
+      return false;
+    }
+  }
+}
+
+function startMatchmakingHandler(req, res){
+  /*
+     Check to see if there is already a match available for you. Otherwise
+     add you to the queue.
+
+     Note: You aren't actually "searching" the queue, because searching a
+     queue costs O(n) time (at least in the worst case). You get added to the
+     queue and wait "passively" for another player to dequeue you.
+
+     There are a few cases you must consider before enqueueing yourself.
+     - Have you been enqueued already, but were dequeued?
+     - Have you been enqueued already?
+     - Can I find a match for you without enqueuing you?
+
+     If no to all of these ^ then I'll enqueue you.
+
+  */
+  var mmr = req.body.mmr;
+  var id = req.body.id;
+  var sc_manager = new SearchingClientsManager(PROPERTIES);
+  var mp_manager = new MatchPairingManager(PROPERTIES);
+  var ds_manager = new DataStoreManager(PROPERTIES, sc_manager, mp_manager);
+  var bin_key = ds_manager.getBinKey(mmr);
+  var paired_id = mp_manager.hasBeenDequeued(id)
+  var is_queued = sc_manager.getEnqueued(id);
+  console.log('Checking if already dequeued');
+  if (paired_id){
+    res.status(200);
+    res.json({response:'You were already queued, also, someone has matched with you'})
+  }
+  if (is_queued){
+    res.status(403);
+    res.json({response: 'You are already queued. Please wait for a match to be found.'})
+  }
+
+  else{
+    var competitor = ds_manager.getBinnedCompetitor(bin_key, id);
+    if (competitor){
+      res.status(200);
+      res.json({response: 'I have a match for you already, no need to be in the queue.'})
+    }
+    else{
+      ds_manager.enqueueId(bin_key, id, mmr)
+      res.status(200);
+      res.json({response: 'You have been queued.'});
+    }
+  }
+
+}
+
+module.exports.startMatchmakingHandler = startMatchmakingHandler;
+module.exports.MatchPairingManager = MatchPairingManager;
+module.exports.SearchingClientsManager = SearchingClientsManager;
+module.exports.DataStoreManager = DataStoreManager;
+module.exports.PROPERTIES = PROPERTIES;
---
+++ app/controllers/tests/test_matchmaking_start.js
@@ -0,0 +1,493 @@
+var assert = require('assert');
+    mm = require('../matchmaking');
+    queue = require('../../structs/queue');
+    http = require('http');
+    config = require('../../../config');
+    hostname = config.host;
+    port = config.port;
+    server = require('../../server');
+    sinon = require('sinon');
+
+
+describe('Helper function unit tests (getters and setters)', function () {
+  /*
+    The manager classes all have specific "getSomething", "setSomething"
+    functions which are wrappers around looking up and retrieving info from
+    the data stores. This module is for testing those functions for each
+    of those clases.
+  */
+
+  var sandbox;
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+    this.properties = new Object();
+    this.properties.BINNED_QUEUES = {'0-750':new queue.Queue()};
+    this.properties.ENQUEUED_PLAYERS = new Set();
+    this.properties.DEQUEUED_PLAYERS = {};
+    this.properties.MMR_LOOKUP = ['0-750'];
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it('test getBinKey gets the bin key', function(done){
+    var properties = new Object();
+    properties.MMR_LOOKUP = ['This is an example of a binned queues key'];
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    actual = ds_manager.getBinKey(0);
+    expected = 'This is an example of a binned queues key'
+    assert(actual == expected);
+    done();
+  });
+
+
+  it('test getEnqueued example', function(done){
+    var properties = new Object();
+    properties.ENQUEUED_PLAYERS = new Set(['an example']);
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    actual = sc_manager.getEnqueued('an example');
+    expected = true;
+    assert(actual == expected);
+    done();
+  });
+
+  it('test getEnqueued calls "has" on searching players set', function(done){
+    var properties = new Object();
+    var mock_set = sinon.stub();
+    properties.ENQUEUED_PLAYERS = sinon.stub();
+    properties.ENQUEUED_PLAYERS.has = mock_set;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    sc_manager.getEnqueued('some_id');
+    assert(mock_set.calledOnce);
+    done();
+  });
+
+  it('test getEnqueued returns true when a player has been enqueued', function(done){
+    var properties = new Object();
+    var mock_set = sinon.stub();
+    mock_set.returns(true);
+    properties.ENQUEUED_PLAYERS = sinon.stub();
+    properties.ENQUEUED_PLAYERS.has = mock_set;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    actual = sc_manager.getEnqueued('some_id');
+    expected = true;
+    assert(actual == expected);
+    done();
+  });
+
+  it('test hasBeenDequeued example', function(done){
+    var properties = new Object();
+    properties.DEQUEUED_PLAYERS = {'example_id': 'paired_example_id'}
+    var pm_manager = new mm.MatchPairingManager(properties);
+    actual = pm_manager.hasBeenDequeued('example_id');
+    expected = 'paired_example_id';
+    assert(actual == expected);
+    done();
+  });
+
+  it('test enqueuedToBin example', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    player_node = {'data':{'id':'1'}};
+    ds_manager.enqueueToBin(bin_key, player_node);
+    //this.properties.BINNED_QUEUES[bin_key].enqueue(player_node);
+    done();
+  });
+
+  it('test enqueuedToBin calls enqueue method of queue', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    mock_q = sinon.stub();
+    mock_q.enqueue = sinon.stub();
+    this.properties.BINNED_QUEUES[bin_key] = mock_q;
+    player_node = {'data':{'id':'1'}};
+    ds_manager.enqueueToBin(bin_key, player_node);
+    assert(mock_q.enqueue.calledOnce);
+    done();
+  });
+
+  it('test enqueuedToBin calls enqueue method with correct args', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    mock_q = sinon.stub();
+    mock_q.enqueue = sinon.stub();
+    this.properties.BINNED_QUEUES[bin_key] = mock_q;
+    player_node = {'data':{'id':'1'}};
+    ds_manager.enqueueToBin(bin_key, player_node);
+    assert(mock_q.enqueue.calledWith(player_node));
+    done();
+  });
+
+});
+
+describe('DataStoreManager unit tests', function () {
+  /*
+    Besides the getter/setter functions already tested above ^, this will
+    test the other functions ('getBinnedCompetitor and 'enqueueId'). I'm
+    essentially testing the order of getters/setter calls and that they
+    are used under certain circumstances.
+  */
+
+  var sandbox;
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+    this.properties = new Object();
+    this.properties.BINNED_QUEUES = {'0-750':new queue.Queue()};
+    this.properties.ENQUEUED_PLAYERS = new Set();
+    this.properties.DEQUEUED_PLAYERS = {};
+    this.properties.MMR_LOOKUP = ['0-750'];
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+  it('test getBinnedCompetitor example', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1'
+    ds_manager.getBinnedCompetitor(bin_key, id)
+    done();
+  });
+
+  it('test getBinnedCompetitor calls isQueueHead once', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1'
+    var iqh = sinon.stub(ds_manager,'isQueueHead');
+    ds_manager.getBinnedCompetitor(bin_key, id)
+    assert(iqh.calledWith(bin_key, id));
+    done();
+  });
+
+  it('test getBinnedCompetitor calls isQueueHead first', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1'
+    var iqh = sinon.stub(ds_manager,'isQueueHead');
+    var dq_bin = sinon.stub(ds_manager, 'dequeueFromBin');
+    ds_manager.getBinnedCompetitor(bin_key, id)
+    sinon.assert.callOrder(iqh, dq_bin);
+    done();
+  });
+
+  it('test getBinnedCompetitor does not call setDequeued if client is head', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1'
+    var iqh = sinon.stub(ds_manager,'isQueueHead');
+    var set_dq = sinon.stub(pm_manager, 'setDequeued');
+    iqh.returns(true);
+    ds_manager.getBinnedCompetitor(bin_key, id)
+    assert(set_dq.notCalled);
+    done();
+  });
+
+  it('test getBinnedCompetitor calls setEnqueued twice if dequeues a client', function(done){
+    /*
+      getBinnedCompetitor is the only method which makes the call to dequeue
+      a client (dequeue === finding a pair for a match). When this happens,
+      I want to make sure that the service keeps track of both clients.
+      setDequeued should be called for both clients.
+    */
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1'
+    var iqh = sinon.stub(ds_manager,'isQueueHead');
+    var dq_bin = sinon.stub(ds_manager, 'dequeueFromBin');
+    var set_dq = sinon.stub(pm_manager, 'setDequeued');
+
+    iqh.returns(false);
+    dq_bin.returns({'data':{'id':'some_id'}});
+    ds_manager.getBinnedCompetitor(bin_key, id)
+    sinon.assert.calledTwice(set_dq);
+    done();
+  });
+
+
+  it('test getBinnedCompetitor calls setEnqueued with both clients info', function(done){
+    /*
+      getBinnedCompetitor is the only method which makes the call to dequeue
+      a client (dequeue === finding a pair for a match). When this happens,
+      I want to make sure that the service keeps track of both clients.
+      setDequeued should be called for with both clients' info. It's also
+      important to understand that when the startMatchmakingWrapper
+      is called with the API and if a client has been "setDequeued", the
+      service doesn't try to queue that client.. and will send a 200 status
+    */
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1'
+    var iqh = sinon.stub(ds_manager,'isQueueHead');
+    var dq_bin = sinon.stub(ds_manager, 'dequeueFromBin');
+    var set_dq = sinon.stub(pm_manager, 'setDequeued');
+
+    iqh.returns(false);
+    dq_bin.returns({'data':{'id':'some_id'}});
+    ds_manager.getBinnedCompetitor(bin_key, id)
+    assert(set_dq.withArgs(id, 'some_id').calledOnce)
+    assert(set_dq.withArgs('some_id', id).calledOnce)
+    done();
+  });
+
+  it('test getBinnedCompetitor does not call setEnqueued case', function(done){
+    /*
+      When getBinnedCompetitor doesn't find another client in the queue,
+      make sure it doesn't setDequeue.
+    */
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1'
+    var iqh = sinon.stub(ds_manager,'isQueueHead');
+    var dq_bin = sinon.stub(ds_manager, 'dequeueFromBin');
+    var set_dq = sinon.stub(pm_manager, 'setDequeued');
+    iqh.returns(false);
+    dq_bin.returns(undefined);
+    ds_manager.getBinnedCompetitor(bin_key, id)
+    assert(set_dq.notCalled);
+    done();
+  });
+
+
+  it('test enqueueId example', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1';
+    mmr = 750;
+    ds_manager.enqueueId(bin_key, id, mmr)
+    done();
+  });
+
+  it('test enqueueId calls getEnqueued', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1';
+    mmr = 750;
+    ge = sinon.stub(sc_manager, 'getEnqueued');
+    ds_manager.enqueueId(bin_key, id, mmr)
+    assert(ge.calledOnce);
+    done();
+  });
+
+  it('test enqueueId does not mark client as enqueued when getEnqueue returns true', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1';
+    mmr = 750;
+    ge = sinon.stub(sc_manager, 'getEnqueued');
+    set_enq = sinon.stub(sc_manager, 'setEnqueued');
+    ge.returns(true);
+    ds_manager.enqueueId(bin_key, id, mmr)
+    assert(set_enq.notCalled);
+    done();
+  });
+
+  it('test enqueueId does not create player node when getEnqueue returns true', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1';
+    mmr = 750;
+    ge = sinon.stub(sc_manager, 'getEnqueued');
+    set_enq = sinon.stub(ds_manager, 'createPlayerNode');
+    ge.returns(true);
+    ds_manager.enqueueId(bin_key, id, mmr)
+    assert(set_enq.notCalled);
+    done();
+  });
+
+  it('test enqueueId does not enqueue a client when getEnqueue returns true', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1';
+    mmr = 750;
+    ge = sinon.stub(sc_manager, 'getEnqueued');
+    set_enq = sinon.stub(ds_manager, 'enqueueToBin');
+    ge.returns(true);
+    ds_manager.enqueueId(bin_key, id, mmr)
+    assert(set_enq.notCalled);
+    done();
+  });
+
+  it('test enqueueId does mark client as enqueued when getEnqueue returns false', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1';
+    mmr = 750;
+    ge = sinon.stub(sc_manager, 'getEnqueued');
+    set_enq = sinon.stub(sc_manager, 'setEnqueued');
+    ge.returns(false);
+    ds_manager.enqueueId(bin_key, id, mmr)
+    assert(set_enq.calledOnce);
+    done();
+  });
+
+  it('test enqueueId does create player node when getEnqueue returns false', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1';
+    mmr = 750;
+    ge = sinon.stub(sc_manager, 'getEnqueued');
+    set_enq = sinon.stub(ds_manager, 'createPlayerNode');
+    set_enq.returns({'data':{'id':id}})
+    ge.returns(false);
+    ds_manager.enqueueId(bin_key, id, mmr)
+    assert(set_enq.calledOnce);
+    done();
+  });
+
+  it('test enqueueId does enqueue a client when getEnqueue returns false', function(done){
+    var properties = this.properties;
+    var sc_manager = new mm.SearchingClientsManager(properties);
+    var pm_manager = new mm.MatchPairingManager(properties);
+    var ds_manager = new mm.DataStoreManager(properties, sc_manager, pm_manager);
+    bin_key = '0-750';
+    id = '1';
+    mmr = 750;
+    ge = sinon.stub(sc_manager, 'getEnqueued');
+    set_enq = sinon.stub(ds_manager, 'enqueueToBin');
+    ge.returns(false);
+    ds_manager.enqueueId(bin_key, id, mmr)
+    assert(set_enq.calledOnce);
+    done();
+  });
+
+});
+
+
+
+
+
+
+describe('Matchmaking API full example', function () {
+  before(function () {
+    /* Setup fake request and response objects for 2 clients */
+    this.request_one = {
+      'body':{'id':1,'mmr':750}
+    };
+    this.request_two= {
+      'body':{'id':2,'mmr':750}
+    };
+    this.response_one = {};
+    this.response_one.send = function(){};
+    this.response_one.status = function(){};
+    this.response_one.json = function(){};
+    this.response_two = {};
+    this.response_two.send = function(){};
+    this.response_two.status = function(){};
+    this.response_two.json = function(){};
+  });
+
+  it('test that a full example has no errors.', function(done){
+    // First client starts matchmaking
+    mm.startMatchmakingHandler(this.request_one, this.response_one);
+    // Second client starts matchmaking
+    mm.startMatchmakingHandler(this.request_two, this.response_two);
+    // Both clients poll their status
+    mm.statusHandler(this.request_one, this.response_one);
+    mm.statusHandler(this.request_two, this.response_two);
+    // They should have been matched together. Now they have necessary
+    // info about their match and should update their status.
+    // Once updated, they can start another matchmaking request.
+    mm.updateStatusHandler(this.request_one, this.response_one);
+    mm.updateStatusHandler(this.request_two, this.response_two);
+    done();
+  })
+
+});
+
+describe('startMatchmakingHandler', function () {;
+  before(function () {
+    /* Setup fake request and response objects for 2 clients */
+    this.request_one = {
+      'body':{'id':1,'mmr':750}
+    };
+    this.request_two= {
+      'body':{'id':2,'mmr':750}
+    };
+    this.response_one = {};
+    this.response_one.send = function(){};
+    this.response_one.status = function(){};
+    this.response_one.json = function(){};
+    this.response_two = {};
+    this.response_two.send = function(){};
+    this.response_two.status = function(){};
+    this.response_two.json = function(){};
+    this.mock_status_one = sinon.spy(this.response_one,'status');
+    this.mock_status_two = sinon.spy(this.response_two,'status');
+    this.mock_response_one = sinon.spy(this.response_one,'json');
+    this.mock_response_two = sinon.spy(this.response_two,'json');
+
+  });
+
+  it('test that binned queue is not empty after first start MM request', function(done){
+    // Each binned queue starts as empty, it should queue the first
+    // client that makes a startMatchmakingHandler request..
+    mm.startMatchmakingHandler(this.request_one, this.response_one);
+    assert(mm.BINNED_QUEUES['0-750'].head != null);
+    done();
+  });
+
+  it('test that binned queue is empty after only two start MM requests', function(done){
+    // Since two start mm requests are made, the binned queue will dequeue
+    // the first request and pair it with the second.
+    mm.startMatchmakingHandler(this.request_one, this.response_one);
+    mm.startMatchmakingHandler(this.request_two, this.response_two);
+    assert(mm.BINNED_QUEUES['0-750'].head == null);
+    done();
+  });
+
+});
+
